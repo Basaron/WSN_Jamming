29b744f004679b69987d3c85a81808555a2223b4
Bastian Aron Kramer <bastian4444@live.dk>
2022-12-01 12:46:39 +0100

    First step

---
+++ Client/client_comm.c
@@ -0,0 +1,46 @@
+
+#include "contiki.h"
+#include "net/netstack.h"
+#include "net/nullnet/nullnet.h"
+
+#include <string.h>
+#include <stdio.h> /* For printf() */
+
+int msgCount = 0;
+
+//static linkaddr_t dest_addr ={{ 0x98, 0xa0, 0x93, 0x1c, 0x00, 0x74, 0x12, 0x00 }};
+/*---------------------------------------------------------------------------*/
+PROCESS(client_comm, "client_comm");
+AUTOSTART_PROCESSES(&client_comm);
+
+/*---------------------------------------------------------------------------*/
+void input_callback(const void *data, uint16_t len,
+  const linkaddr_t *src, const linkaddr_t *dest)
+{
+  if(len == sizeof(unsigned)) {
+    unsigned msg;
+    memcpy(&msg, data, sizeof(msg));
+    
+    printf("The message resived is: %u \n", msg);
+    msgCount ++;
+
+    static int ackmsg = 1;
+    nullnet_buf = (uint8_t *)&ackmsg;
+    nullnet_len = sizeof(ackmsg);
+    NETSTACK_NETWORK.output(&src);
+    
+    
+  }
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(client_comm, ev, data)
+{
+  PROCESS_BEGIN();  
+  
+  nullnet_set_input_callback(input_callback);
+
+  printf("Ready to recive\n");
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
---
+++ Server/build/sky/obj/.deps/nullradio.d
@@ -0,0 +1,7 @@
+build/sky/obj/nullradio.o: /home/vagrant/contiki-ng/os/dev/nullradio.c \
+ /home/vagrant/contiki-ng/os/dev/nullradio.h \
+ /home/vagrant/contiki-ng/os/dev/radio.h
+
+/home/vagrant/contiki-ng/os/dev/nullradio.h:
+
+/home/vagrant/contiki-ng/os/dev/radio.h:
