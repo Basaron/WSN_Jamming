3e2a9f448de448617549c7367526cba8e23db6c4
emilhu20 <emil.hu@hotmail.com>
2022-12-04 01:53:13 +0100

    added comments

--- Server/server_comm.c
+++ Server/server_comm.c
@@ -1,4 +1,6 @@
 
+/* IMPLEMENTATION OF SERVER - responsible for sending messages*/
+
 #include "contiki.h"
 #include "net/netstack.h"
 #include "net/nullnet/nullnet.h"
@@ -6,61 +8,68 @@
 #include <string.h>
 #include <stdio.h> /* For printf() */
 
-bool ackresived = false;
-int failMsgCount = 0;
-int startchannel = 26;
-int resivedMsgCount = 0;
+
+/*-----------------------------CONFIGURATION-----------------------------*/
 #define sequence = {1,5,7,9.13}:
-/* Configuration */
 #define SEND_INTERVAL (8 * CLOCK_SECOND)
 
 //This adress {{ 0x98, 0xa0, 0x93, 0x1c, 0x00, 0x74, 0x12, 0x00 }};
-static linkaddr_t dest_addr = {{ 0xd6, 0x73, 0x93, 0x1c, 0x00, 0x74, 0x12, 0x00 }};
+static linkaddr_t dest_addr = {{ 0xd6, 0x73, 0x93, 0x1c, 0x00, 0x74, 0x12, 0x00 }};  //addresses
 
+//MAC??
 #if MAC_CONF_WITH_TSCH
 #include "net/mac/tsch/tsch.h"
 static linkaddr_t coordinator_addr = {{ 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }};
 #endif /* MAC_CONF_WITH_TSCH */
 
+//state message variables
+bool ackReceived = false;
+int failMsgCount = 0;
+int startChannel = 26;
+int receivedMsgCount = 0;
+
 /*---------------------------------------------------------------------------*/
 PROCESS(server_comm, "Server_comm");
 AUTOSTART_PROCESSES(&server_comm);
+
 //NETSTACK_RADIO.on();
 //NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL,11);
-/*---------------------------------------------------------------------------*/
-void input_callback(const void *data, uint16_t len,
-  const linkaddr_t *src, const linkaddr_t *dest)
-{
+
+
+/*-------------------------------INPUT TO SERVER CALLBACK---------------------------------------*/
+void input_callback(const void *data, uint16_t len, const linkaddr_t *src, const linkaddr_t *dest)
+{ 
+  //only receive message of valid type 
   if(len == sizeof(unsigned)) {
     unsigned msg;
-    memcpy(&msg, data, sizeof(msg));
-    
+    memcpy(&msg, data, sizeof(msg));                  //set data 
     printf("The acknowledgement message resived \n");
-
-    resivedMsgCount ++;
-
-    ackresived = true;
+    
+    //update state 
+    receivedMsgCount++;
+    ackReceived = true;
 
   }
 }
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(server_comm, ev, data)
 {
-  
-  static struct etimer periodic_timer;
-  static int msgCount = 0;
+  static struct etimer periodic_timer;  //initialize timer
+  static int msgCount = 0;              //initialize message count 
+
   PROCESS_BEGIN();
 
-#if MAC_CONF_WITH_TSCH
-  if (linkaddr_cmp(&coordinator_addr,&linkaddr_node_addr)) {
-    printf("Server set as coordinator");
-    tsch_set_coordinator(true);
-  }
-  else {
-    printf("Server not set as coordinator");
-  }
-  //tsch_set_coordinator(linkaddr_cmp(&coordinator_addr, &linkaddr_node_addr)); //make coordinator if equal
-#endif /* MAC_CONF_WITH_TSCH */
+  //aner ik hvad der sker her 
+  #if MAC_CONF_WITH_TSCH
+    if (linkaddr_cmp(&coordinator_addr,&linkaddr_node_addr)) {
+      printf("Server set as coordinator");
+      tsch_set_coordinator(true);
+    }
+    else {
+      printf("Server not set as coordinator");
+    }
+    //tsch_set_coordinator(linkaddr_cmp(&coordinator_addr, &linkaddr_node_addr)); //make coordinator if equal
+  #endif /* MAC_CONF_WITH_TSCH */
 
 
   /* Initialize NullNet */
@@ -68,42 +77,50 @@ PROCESS_THREAD(server_comm, ev, data)
   nullnet_len = sizeof(msgCount);
   nullnet_set_input_callback(input_callback);
 
-  if(!linkaddr_cmp(&dest_addr, &linkaddr_node_addr)) { //If not equal
+  //If addresses not equal
+  if(!linkaddr_cmp(&dest_addr, &linkaddr_node_addr)) { 
     printf("LLAddress and DestAddr not equal. Proceeding..");
     etimer_set(&periodic_timer, SEND_INTERVAL);
+    
+    
     while(1) {
       PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
-      if (ackresived == true)
+      
+      //check if message is received 
+      if (ackReceived == true)
       {
         failMsgCount = 0;
-      }else
-      {
+      } else {
         failMsgCount ++;
-        printf("No acknowledgement message resived before timeout \n");
+        printf("No acknowledgement message received before timeout \n");
       }
-      if (failMsgCount >= 5) { //change to failmsg
+
+      //if failmsgcount is above boundary, start channel hopping on given sequence
+      if (failMsgCount >= 5) { 
         printf("Attempting to change channel");
-        startchannel++;
-        if (NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL, startchannel) != RADIO_RESULT_OK){
-          printf("issue changing channel");
+        startChannel++;                             //change to next channel 
+
+        //try to change channel 
+        if (NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL, startChannel) != RADIO_RESULT_OK){
+          printf("Issue changing channel");
         }
         else {
-          printf("\n Succesfully changed channel to %u",startchannel);
+          printf("\n Succesfully changed channel to %u",startChannel);
           failMsgCount = 0; //reset msg count
         }
       }
-      //if failmsgcount is above boundary, start channel hopping on given sequence
 
-
-      printf("sending %u to client \n", msgCount);
+      //transmitting message 
+      printf("Sending %u to client \n", msgCount);
       NETSTACK_NETWORK.output(&dest_addr);
       msgCount++;
-      ackresived = false;
+      ackReceived = false;
 
+      //reset timer 
       etimer_reset(&periodic_timer);
     }
   }
-  printf("Dest and Current addr is equal");
+  
+  printf("Destination and current address are equal");
   PROCESS_END();
 }
-/*---------------------------------------------------------------------------*/
--- jammer/constant_jammer.c
+++ jammer/constant_jammer.c
@@ -1,16 +1,11 @@
 #include "contiki.h"
-
 #include "random.h"
-
 #include "net/netstack.h"
 #include "sys/rtimer.h"
 #include "dev/button-sensor.h"
 #include "sys/energest.h"
-
 #include "dev/leds.h"
-
 #include "./cc2420.h"
-
 #include <stdio.h>
 #include <string.h>
 
@@ -20,10 +15,13 @@
 #define JAMMER_PACKET_LEN 120
 #define ENERGEST_CONF_ON 1
 
+//struct to describe data packet 
 typedef struct{
      char data[JAMMER_PACKET_LEN];
 }jpacket_t;
 
+
+//function to convert time to seconds 
 static unsigned long to_seconds(uint64_t time) {
   return (unsigned long)(time / ENERGEST_SECOND);
 }
@@ -42,20 +40,22 @@ PROCESS_THREAD(jammerProcess, ev, data)
   //TODO: Use timers to handle loop
   //Work only with radio activation time
 
-  //set timer..
-  etimer_set(&periodic_timer, CLOCK_SECOND * 10);
-  //set channel..
-  cc2420_set_channel(26);
+  etimer_set(&periodic_timer, CLOCK_SECOND * 10);   //set timer 
+  cc2420_set_channel(26);                           //set channel
 
   //Load data into packet
-  jpacket_t jpacket;
-  memset(&jpacket, 0, sizeof(jpacket_t));
-  strcpy(jpacket.data, "Antonio Gonga is taking down your network.");
+  jpacket_t jpacket;                                                      
+  memset(&jpacket, 0, sizeof(jpacket_t));                                  
+  strcpy(jpacket.data, "Antonio Gonga is taking down your network.");     
     
+
   //Constant jammer - jams with fastest interval possible  
   while(1) {
      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
+     
+     //resets timer 
      etimer_reset(&periodic_timer);
+
      //CALL Radio Driver to transmit the Packet..
      cc2420_driver.send((void*)&jpacket, JAMMER_PACKET_LEN); 
      
