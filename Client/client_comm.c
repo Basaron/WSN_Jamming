90e9d7536f196632c370a9b6dd24bf4f2df66a1f
Bastian Aron Kramer <bastian4444@live.dk>
2022-12-05 14:54:42 +0100

    merget working code with commens

--- Client/client_comm.c
+++ Client/client_comm.c
@@ -1,42 +1,40 @@
 
-/* IMPLEMENTATION OF CLIENT - responsible for receiving messages */
-
 #include "contiki.h"
 #include "net/netstack.h"
 #include "net/nullnet/nullnet.h"
+#include <os/dev/radio.h> 
+
 #include <string.h>
 #include <stdio.h> /* For printf() */
 
-#define EXPECTED_REC_INTERVAL (8 * CLOCK_SECOND)
-
-
 //state message variables
-bool received = false;
+bool recieved = false;
 int msgCount = 0;
-int failCount = 0;
-int startChannel = 26;
-//static linkaddr_t dest_addr ={{ 0x98, 0xa0, 0x93, 0x1c, 0x00, 0x74, 0x12, 0x00 }}; 
+int failcount = 0;
+int startchannel = 26;
 
+#define EXPECTED_REC_INTERVAL (4 * CLOCK_SECOND)
 
+//static linkaddr_t dest_addr ={{ 0x98, 0xa0, 0x93, 0x1c, 0x00, 0x74, 0x12, 0x00 }};
 /*---------------------------------------------------------------------------*/
 PROCESS(client_comm, "client_comm");
 AUTOSTART_PROCESSES(&client_comm);
 
-
 /*--------------------------CALLBACK TO RECEIVE MESSAGES-------------------------------------------------*/
-void input_callback(const void *data, uint16_t len, const linkaddr_t *src, const linkaddr_t *dest) 
+void input_callback(const void *data, uint16_t len,
+  const linkaddr_t *src, const linkaddr_t *dest)
 {
-  //only receive messages that are unsigned i.e non-negative 
+   //only receive messages that are unsigned i.e non-negative 
   if(len == sizeof(unsigned)) {
-    received = true;
+    recieved = true;
     unsigned msg;
-    memcpy(&msg, data, sizeof(msg));                //set received data 
+    memcpy(&msg, data, sizeof(msg));
     
     //incrementing message count 
-    printf("The message received is: %u \n", msg);  //print message 
+    printf("The message resived is: %u \n", msg);
     msgCount ++;
 
-    //acknoledgement messages 
+    //acknoledgement messages
     static int ackmsg = 1;
     nullnet_buf = (uint8_t *)&ackmsg;
     nullnet_len = sizeof(ackmsg);
@@ -49,45 +47,52 @@ void input_callback(const void *data, uint16_t len, const linkaddr_t *src, const
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(client_comm, ev, data)
 {
-  PROCESS_BEGIN();  
+  PROCESS_BEGIN();
+
   static struct etimer periodic_timer;                //initialize timer
   nullnet_set_input_callback(input_callback);         //set callback
   etimer_set(&periodic_timer, EXPECTED_REC_INTERVAL); //set timer
+  
+  NETSTACK_RADIO.on();
+  NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL,startchannel);
 
   while(1) {
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
     
     //check for received message 
-    if (received) {
-      failCount = 0;
+    if (recieved) {
+      failcount = 0;
     }
     else {
-      printf("Did not receive expected message. Incrementing count\n");
-      failCount++;
+      printf("Didnt recieve expected message. Incrementing count \n");
+      //failcount++;
     }
-
+    
     //change channel if too many failed messages (result of succesfull jamming) 
-    if (failCount >= 5){
-      printf("Too many non-received messages. Swapping channel");
-      startChannel++;                                                 //try the next channel 
-      
+    if (failcount >= 5){
+      printf("Too many non-recieved messages. Swapping channel \n");
+      startchannel++;
+
       //try to change channel 
-      if (NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL, startChannel) != RADIO_RESULT_OK) {
-        printf("Issue changing channel");
+      if (NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL, startchannel) != RADIO_RESULT_OK){
+        printf("issue changing channel \n");
       }
       else {
-        printf("\n Succesfully changed channel to %d", startChannel);
-        failCount = 0;                                                  //reset
-        nullnet_set_input_callback(input_callback);                     //trying to reinstantiate
+        printf("\n Succesfully changed channel to %d \n",startchannel);
+        failcount = 0; //reset
+        nullnet_set_input_callback(input_callback); //trying to reinstantiate
 
       }
     }
+    recieved = false;
 
     //reset timer 
     etimer_reset(&periodic_timer);
   }
 
-  printf("Ready to receive\n");
+
+  printf("Ready to recive \n");
+
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
--- Server/server_comm.c
+++ Server/server_comm.c
@@ -1,20 +1,26 @@
 
-/* IMPLEMENTATION OF SERVER - responsible for sending messages*/
-
 #include "contiki.h"
 #include "net/netstack.h"
 #include "net/nullnet/nullnet.h"
-#include <os/dev/radio.h>  
+#include <os/dev/radio.h>
+
 #include <string.h>
 #include <stdio.h> /* For printf() */
 
+//state message variables
+bool ackresived = false;
+int failMsgCount = 0;
+int startchannel = 26;
+int resivedMsgCount = 0;
+int testLenge = 50; 
+#define sequence = {1,5,7,9.13}:
 
 /*-----------------------------CONFIGURATION-----------------------------*/
-#define sequence = {1,5,7,9.13}:
-#define SEND_INTERVAL (8 * CLOCK_SECOND)
+#define SEND_INTERVAL (4 * CLOCK_SECOND)
 
-//This adress {{ 0x98, 0xa0, 0x93, 0x1c, 0x00, 0x74, 0x12, 0x00 }};
-static linkaddr_t dest_addr = {{ 0xd6, 0x73, 0x93, 0x1c, 0x00, 0x74, 0x12, 0x00 }};  //addresses
+//static linkaddr_t dest_addr = {{ 0x98, 0xa0, 0x93, 0x1c, 0x00, 0x74, 0x12, 0x00 }};
+//static linkaddr_t dest_addr = {{ 0xd6, 0x73, 0x93, 0x1c, 0x00, 0x74, 0x12, 0x00 }};
+static linkaddr_t dest_addr = {{ 0x36, 0x29, 0x93, 0x1c, 0x00, 0x74, 0x12, 0x00 }};
 
 //MAC??
 #if MAC_CONF_WITH_TSCH
@@ -22,54 +28,52 @@ static linkaddr_t dest_addr = {{ 0xd6, 0x73, 0x93, 0x1c, 0x00, 0x74, 0x12, 0x00 
 static linkaddr_t coordinator_addr = {{ 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }};
 #endif /* MAC_CONF_WITH_TSCH */
 
-//state message variables
-bool ackReceived = false;
-int failMsgCount = 0;
-int startChannel = 26;
-int receivedMsgCount = 0;
-
 /*---------------------------------------------------------------------------*/
 PROCESS(server_comm, "Server_comm");
 AUTOSTART_PROCESSES(&server_comm);
 
-//NETSTACK_RADIO.on();
-//NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL,11);
-
-
 /*-------------------------------INPUT TO SERVER CALLBACK---------------------------------------*/
-void input_callback(const void *data, uint16_t len, const linkaddr_t *src, const linkaddr_t *dest)
-{ 
+void input_callback(const void *data, uint16_t len,
+  const linkaddr_t *src, const linkaddr_t *dest)
+{
   //only receive message of valid type 
   if(len == sizeof(unsigned)) {
     unsigned msg;
-    memcpy(&msg, data, sizeof(msg));                  //set data 
-    printf("The acknowledgement message resived \n");
-    
-    //update state 
-    receivedMsgCount++;
-    ackReceived = true;
+    memcpy(&msg, data, sizeof(msg));
+    if (msg == 1)
+    {
+      printf("The acknowledgement message resived \n");
+
+      //update state 
+      resivedMsgCount ++;
+      ackresived = true;
+    }
 
   }
 }
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(server_comm, ev, data)
 {
+  
   static struct etimer periodic_timer;  //initialize timer
   static int msgCount = 0;              //initialize message count 
+  static int i = 0;
 
   PROCESS_BEGIN();
 
-  //aner ik hvad der sker her 
-  #if MAC_CONF_WITH_TSCH
-    if (linkaddr_cmp(&coordinator_addr,&linkaddr_node_addr)) {
-      printf("Server set as coordinator");
-      tsch_set_coordinator(true);
-    }
-    else {
-      printf("Server not set as coordinator");
-    }
-    //tsch_set_coordinator(linkaddr_cmp(&coordinator_addr, &linkaddr_node_addr)); //make coordinator if equal
-  #endif /* MAC_CONF_WITH_TSCH */
+  NETSTACK_RADIO.on();
+  NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL,startchannel); 
+
+#if MAC_CONF_WITH_TSCH
+  if (linkaddr_cmp(&coordinator_addr,&linkaddr_node_addr)) {
+    printf("Server set as coordinator");
+    tsch_set_coordinator(true);
+  }
+  else {
+    printf("Server not set as coordinator");
+  }
+  //tsch_set_coordinator(linkaddr_cmp(&coordinator_addr, &linkaddr_node_addr)); //make coordinator if equal
+#endif /* MAC_CONF_WITH_TSCH */
 
 
   /* Initialize NullNet */
@@ -78,49 +82,55 @@ PROCESS_THREAD(server_comm, ev, data)
   nullnet_set_input_callback(input_callback);
 
   //If addresses not equal
-  if(!linkaddr_cmp(&dest_addr, &linkaddr_node_addr)) { 
-    printf("LLAddress and DestAddr not equal. Proceeding..");
+  if(!linkaddr_cmp(&dest_addr, &linkaddr_node_addr)) { //If not equal
+    printf("LLAddress and DestAddr not equal. Proceeding..\n");
     etimer_set(&periodic_timer, SEND_INTERVAL);
     
-    
-    while(1) {
+    while(i <= testLenge) {
       PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
       
       //check if message is received 
-      if (ackReceived == true)
+      if (ackresived)
       {
         failMsgCount = 0;
-      } else {
-        failMsgCount ++;
-        printf("No acknowledgement message received before timeout \n");
+      }else
+      {
+        //failMsgCount ++;
+        printf("No acknowledgement message resived before timeout \n");
       }
 
       //if failmsgcount is above boundary, start channel hopping on given sequence
-      if (failMsgCount >= 5) { 
-        printf("Attempting to change channel");
-        startChannel++;                             //change to next channel 
+      if (failMsgCount >= 5) { //change to failmsg
+        printf("Attempting to change channel \n");
+        startchannel++;       //change to next channel 
 
-        //try to change channel 
-        if (NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL, startChannel) != RADIO_RESULT_OK){
-          printf("Issue changing channel");
+        //try to change channel
+        if (NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL, startchannel) != RADIO_RESULT_OK){
+          printf("issue changing channel \n");
         }
         else {
-          printf("\n Succesfully changed channel to %u",startChannel);
+          printf("\n Succesfully changed channel to %d \n",startchannel);
           failMsgCount = 0; //reset msg count
+          nullnet_set_input_callback(input_callback); //trying to reinstantiate
         }
       }
+      //if failmsgcount is above boundary, start channel hopping on given sequence
 
-      //transmitting message 
-      printf("Sending %u to client \n", msgCount);
+      //transmitting message
+      printf("sending %u to client \n", msgCount);
       NETSTACK_NETWORK.output(&dest_addr);
       msgCount++;
-      ackReceived = false;
+      ackresived = false;
+      i++;
 
       //reset timer 
       etimer_reset(&periodic_timer);
     }
   }
+
+  printf("Number of message Send: %d \n",msgCount);
+  printf("Number of message resived: %d \n",resivedMsgCount);
   
-  printf("Destination and current address are equal");
   PROCESS_END();
 }
+/*---------------------------------------------------------------------------*/
